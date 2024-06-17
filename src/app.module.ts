747a0e45ae156bef2b04be75bd5f2d8b3fd2764c
vitormedeiros911 <vitormedeiros911@outlook.com>
2024-06-17 09:32:04 -0300

    Adiciona módulo de carrinho

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CarrinhoModule } from './carrinho/carrinho.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URI),
+    CarrinhoModule,
   ],
 })
 export class AppModule {}
